866e616ef0afb44521cfe68123d71204c6c663a4
Johnexzy <obajohn75@gmail.com>
2021-11-08 11:55:18 +0100

    Added contract: buildspace- lesson 3

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-// SPDX-License-Identifier: UNLICENSED
+
 
 contract WavePortal {
     constructor () {
--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -3,9 +3,9 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+// const hre = require("hardhat");
 
-async function main() {
+ const main = async() => {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
   //
@@ -24,9 +24,14 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
     process.exit(1);
-  });
+  }
+};
+
+runMain();
