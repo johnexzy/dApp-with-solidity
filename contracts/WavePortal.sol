4ad1be1f4640a4ea47390455c1bacffe9beb0b48
Johnexzy <obajohn75@gmail.com>
2021-11-08 11:50:29 +0100

    Added contract: buildspace

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// SPDX-License-Identifier: UNLICENSED
+
+contract WavePortal {
+    constructor () {
+        console.log("Hey, I am a contract and I am smart");
+    }
+}
